c7e08fac64f769b40faec367b7acf2f54fc404a2
May <92554133+qtmay@users.noreply.github.com>
2021-10-14 18:25:21 -0500

    Add files via upload

---
+++ config.php
@@ -0,0 +1,15 @@
+<?php
+	/*
+		<config>
+	*/
+	$DBhost = "localhost";
+	$DBport = 3306;
+	$DBuser = "root";
+	$DBpass = "";
+	$DB = "mayware";
+
+	$mainpage = "users.php"; //index.php redirect
+	/*
+		</config>
+	*/
+?>
