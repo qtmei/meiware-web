85409c9564d27341cf7216aa0e1d7a2d053e10cd
Mei <92554133+qtmei@users.noreply.github.com>
2024-09-02 22:23:10 -0500

    Update functions.php

--- core/functions.php
+++ core/functions.php
@@ -7,8 +7,6 @@
 	$DBuser = "root";
 	$DBpass = "";
 	$DB = "meiware";
-
-	$mainpage = "home.php"; //index.php redirect
 	/*
 	</config>
 	*/
@@ -18,50 +16,38 @@
 	$con = new PDO("mysql:host=$DBhost;port=$DBport;dbname=$DB;charset=utf8", "$DBuser", "$DBpass");
 	$con->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
 
-	function IsValidSession()
-	{
+	function IsValidSession() {
 		global $con;
 		$loggedin = false;
 
-		if(isset($_SESSION["username"]) && isset($_SESSION["password"]))
-		{
+		if(isset($_SESSION["username"]) && isset($_SESSION["password"])) {
 			$sta = $con->prepare("SELECT password FROM accounts WHERE username=:username");
 			$sta->execute(array(':username' => $_SESSION["username"]));
 			$password = $sta->fetch()["password"];
 
-			if($_SESSION["password"] == $password)
-			{
+			if($_SESSION["password"] == $password) {
 				$loggedin = true;
-			}
-			else
-			{
+			} else {
 				session_unset();
 				session_destroy();
 				header("Location: login.php");
-				die();
+				exit();
 			}
 		}
 
 		return $loggedin;
 	}
 
-	function getIP()
-	{
-		if(array_key_exists('HTTP_X_FORWARDED_FOR', $_SERVER) && !empty($_SERVER['HTTP_X_FORWARDED_FOR']))
-		{
-			if (strpos($_SERVER['HTTP_X_FORWARDED_FOR'], ',') > 0)
-			{
+	function getIP() {
+		if(array_key_exists('HTTP_X_FORWARDED_FOR', $_SERVER) && !empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+			if(strpos($_SERVER['HTTP_X_FORWARDED_FOR'], ',') > 0) {
 				$addr = explode(",", $_SERVER['HTTP_X_FORWARDED_FOR']);
 
 				return trim($addr[0]);
-			}
-			else
-			{
+			} else {
 				return $_SERVER['HTTP_X_FORWARDED_FOR'];
 			}
-		}
-		else
-		{
+		} else {
 			return $_SERVER['REMOTE_ADDR'];
 		}
 	}
